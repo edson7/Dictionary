fc11199f2764771c0d03f84042d4f64bf7c1b2a6
“edson.dossantos” <edson.lopes@gmx.de>
2020-03-12 15:05:50 -0400

    Updating  Dictionary Function

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dictionary
 {
@@ -8,6 +9,35 @@ namespace Dictionary
         {
             Console.WriteLine("Using Dictionary");
             Console.ReadKey();
+        }
+        public static Dictionary<char, int> ChaineCaractere(string chaine)
+        {
+            if (chaine == null)
+            {
+                return null;
+            }
+            Dictionary<char, int> dictionnaire = new Dictionary<char, int>();
+
+            string chaineFinale = chaine.ToLower();
+            const int COMPTEUR = 1;
+
+            for (int i = 0; i < chaineFinale.Length; i++)
+            {
+
+                if (char.IsLetterOrDigit(chaineFinale[i]))
+                {
+                    if (!dictionnaire.ContainsKey(chaineFinale[i]))
+                    {
+                        dictionnaire.Add(chaineFinale[i], COMPTEUR);
+                    }
+                    else
+                    {
+                        dictionnaire[chaineFinale[i]] += 1;
+                    }
+                }
+            }
+
+            return dictionnaire;
         }
     }
 }
